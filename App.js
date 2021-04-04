c68471773e02be48196d183b48ea5c0c72989b2d
PhillipRC <phillip.r.cargo@gmail.com>
2021-04-04 10:32:48 -0400

    Added a test client app

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Application to test
+ */
+class App {
+
+  /**
+   * App constructor
+   * @param {*} url WebSocket server
+   * @param {*} outputElement Element to display server messages
+   */
+  constructor(url, outputElement) {
+    console.debug('App:constructor', url = 'ws://localhost:8080')
+    // create a WebSocket
+    this.ws = new WebSocket(url)
+    // listen for message event from WebSocket
+    this.ws.addEventListener('message', (event) => {
+      this.handleServerMessage(event.data, outputElement)
+    })
+  }
+
+  /**
+   * Waits for an event to trigger on an object
+   * @param {*} obj 
+   * @param {string} eventType 
+   * @returns 
+   */
+  waitForEvent = (obj, eventType) => new Promise(function (resolve) {
+    let resolver = (event) => {
+      obj.removeEventListener(eventType, resolver)
+      resolve(event)
+    }
+    obj.addEventListener(eventType, resolver)
+  });
+
+  /**
+   * Send a message via the WebSocket
+   * @param {string} message 
+   */
+  async send(message) {
+    console.debug('App:send', message);
+    if (this.ws.readyState !== WebSocket.OPEN) {
+      console.debug('App:send waiting for WebSocket open event')
+      await this.waitForEvent(this.ws, 'open')
+    }
+    console.debug('App:send > WebSocket.send()', message)
+    this.ws.send(message)
+  }
+
+  /**
+   * 
+   * @param {string} message 
+   * @param {object} outputElement 
+   */
+  handleServerMessage(message, outputElement) {
+    console.debug('App:handleServerMessage', message)
+    if (outputElement) {
+      console.debug('App:handleServerMessage sending to outputElement')
+      outputElement.innerHTML += (message + '\n')
+    }
+  }
+
+}
